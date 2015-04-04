a51ee08acdce4d9a389a317333f723f56ff8498d
mwbeats <mwbeats3@gmail.com>
2015-04-04 14:20:05 -0500

    - Added ObservationDetailActivity class and activity_observation_detail XML file to res/layout and res/menu folder.
    - Added TextView for the observation's description.
    - Added code within the onCreate() of ObservationDetailActivity that allows the user to click on the observation's description and edit it.

--- app/src/main/java/com/alabama/bamboofinder/ObservationDetailActivity.java
+++ app/src/main/java/com/alabama/bamboofinder/ObservationDetailActivity.java
@@ -4,7 +4,11 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+/**
+ * Created by Michael W. on 4/4/2015.
+ */
 
 public class ObservationDetailActivity extends ActionBarActivity {
 
@@ -12,6 +16,11 @@ public class ObservationDetailActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_observation_detail);
+
+        // When the user clicks on the observation's description TextView, the user will
+        // be able to edit the contents of the TextView.
+
+
     }
 
 
