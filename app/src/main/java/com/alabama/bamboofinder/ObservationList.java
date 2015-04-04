d673bf148773e8945c52f1beef92738960d423e2
mwbeats <mwbeats3@gmail.com>
2015-04-04 15:03:43 -0500

    - Added ObservationDetailActivity class and activity_observation_detail XML file to res/layout and res/menu folder.
    - Added TextView for the observation's description.
    - Added code within the onCreate() of ObservationDetailActivity that allows the user to click on the observation's description and edit it.
    
    - Changed the previous TextView object to a EditText object. Allows the user to edit the content of the description.
    
    - Added ObservationListActivity class and activity_observation_list XML file to res/layout and res/menu folder.
    - Added ObservationList class and implemented methods.

---
+++ app/src/main/java/com/alabama/bamboofinder/ObservationList.java
@@ -0,0 +1,26 @@
+package com.alabama.bamboofinder;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Michael W. on 4/4/2015.
+ */
+public class ObservationList {
+
+    private List<Observation> mObservationList;
+
+    public ObservationList() {
+        mObservationList = new LinkedList<>();
+    }
+
+    // adds an observation to the observation list
+
+    public void addObservation(Observation observation) {
+    }
+
+    // removes an observation from the observation list
+
+    public void removeObservation(Observation observation) {
+    }
+}
