386466bec0e67e2345e5a12a66f821c2c395698b
Jeremy Barton <jtbarton1@crimson.ua.edu>
2015-04-06 03:38:34 -0500

    Implemented picture taking in ObservationDetailActivity.

--- app/src/main/java/com/alabama/bamboofinder/ObservationDetailActivity.java
+++ app/src/main/java/com/alabama/bamboofinder/ObservationDetailActivity.java
@@ -1,12 +1,27 @@
 package com.alabama.bamboofinder;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 
 public class ObservationDetailActivity extends ActionBarActivity {
+    private static final int CAMERA_REQUEST = 1888;
+    private ImageView mImageView;
+    private EditText mSpeciesText;
+    private EditText mDescriptionText;
+    private Button mSaveButton;
+    private Button mCancelButton;
+    private Button mValidateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +43,13 @@ public class ObservationDetailActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        switch (id) {
+            case R.id.menu_item_new_picture:
+                mImageView = (ImageView)findViewById(R.id.observationImage);
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+
+        }
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
@@ -35,5 +57,20 @@ public class ObservationDetailActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            Uri imageUri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                bitmap = Bitmap.createScaledBitmap(bitmap, 864, 486, true);
+                mImageView.setImageBitmap(bitmap);
+            }
+            catch (Exception e) {
+                Log.e("Result Exception", e.toString());
+            }
+        }
     }
 }
