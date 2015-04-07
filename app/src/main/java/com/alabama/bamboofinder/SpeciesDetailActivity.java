5fe3db18b7a0e9c12bfb03cde9660ffa13ec61e6
mwbeats <mwbeats3@gmail.com>
2015-04-07 13:15:55 -0500

    - Added EducationalActivity & SpeciesDetailActivity - Added images for EducationalActivity - Edited SpeciesDetailActivity - @1:15 PM

--- app/src/main/java/com/alabama/bamboofinder/SpeciesDetailActivity.java
+++ app/src/main/java/com/alabama/bamboofinder/SpeciesDetailActivity.java
@@ -6,20 +6,36 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
 
 public class SpeciesDetailActivity extends ActionBarActivity {
 
-    //private Spinner mSpeciesSpinner;
-    //private Spinner mGenusSpinner;
+    private Spinner mSpeciesSpinner;
+    private Spinner mGenusSpinner;
     private Button mGenerateInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_species_detail);
+
+        mSpeciesSpinner = (Spinner)findViewById(R.id.species_spinner);
+        mGenusSpinner = (Spinner)findViewById(R.id.genus_spinner);
+
+        ArrayAdapter<CharSequence> arrayAdapterSpecies = ArrayAdapter.createFromResource(this,
+                R.array.species, android.R.layout.simple_spinner_item);
+
+        ArrayAdapter<CharSequence> arrayAdapterGenus = ArrayAdapter.createFromResource(this,
+                R.array.genus, android.R.layout.simple_spinner_item);
+
+        arrayAdapterSpecies.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        arrayAdapterGenus.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        mSpeciesSpinner.setAdapter(arrayAdapterSpecies);
+        mGenusSpinner.setAdapter(arrayAdapterGenus);
 
         findViewById(R.id.species_generate_detail).setOnClickListener(new View.OnClickListener() {
             @Override
