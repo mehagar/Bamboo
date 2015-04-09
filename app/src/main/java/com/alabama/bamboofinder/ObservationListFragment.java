136910c86b0c0cd55000915bfe321c6c65ffaf58
mwbeats <mwbeats3@gmail.com>
2015-04-09 16:12:44 -0500

    - @4:11 PM - 4/9/2015:     Added Fragment class (ObservationListFragment.java & SingleFragmentActivity.java) and fragment xml files (activity_fragment.xml & fragment_observation_list.xml)

---
+++ app/src/main/java/com/alabama/bamboofinder/ObservationListFragment.java
@@ -0,0 +1,29 @@
+package com.alabama.bamboofinder;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ObservationListFragment extends ListFragment {
+
+
+    public ObservationListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_observation_list, container, false);
+    }
+}
