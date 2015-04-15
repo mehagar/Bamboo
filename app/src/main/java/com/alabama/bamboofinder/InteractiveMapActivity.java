04340e8177c53b16dd374c4b51106ab2391be047
mehagar <michaelehagar@gmail.com>
2015-04-15 11:45:52 -0500

    Added debugging code for up button in observation detail activity.

--- app/src/main/java/com/alabama/bamboofinder/InteractiveMapActivity.java
+++ app/src/main/java/com/alabama/bamboofinder/InteractiveMapActivity.java
@@ -11,7 +11,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationRequest;
