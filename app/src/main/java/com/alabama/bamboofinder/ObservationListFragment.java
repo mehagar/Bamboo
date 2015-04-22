1037c5e0c6441873225df809b3f57ba1c57b7c0e
mwbeats <mwbeats3@gmail.com>
2015-04-22 12:00:10 -0500

    Editing ObservationListFragment to execute api calls.

--- app/src/main/java/com/alabama/bamboofinder/ObservationListFragment.java
+++ app/src/main/java/com/alabama/bamboofinder/ObservationListFragment.java
@@ -1,9 +1,14 @@
 package com.alabama.bamboofinder;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.location.Location;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -17,6 +22,9 @@ import android.widget.TextView;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.model.LatLng;
 
+import org.json.JSONObject;
+
+import java.io.InputStream;
 import java.util.List;
 
 /**
@@ -25,9 +33,11 @@ import java.util.List;
 public class ObservationListFragment extends ListFragment {
 
     private static final String TAG = "ObservationListFragment";
-    private List<Observation> mObservations;
+    private static List<Observation> mObservations;
+    private ApiManager api;
 
-    private LatLng mLastUserPosition;
+    // 1. Shared preferences (
+    // 2. Get users string from preferences
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,6 +46,26 @@ public class ObservationListFragment extends ListFragment {
         // needs to receive options menu callbacks
 
         getActivity().setTitle(R.string.title_activity_observation_list);
+
+        SharedPreferences prefs = getActivity().getSharedPreferences("com.alabama.bamboofinder", Context.MODE_PRIVATE);
+        String user = prefs.getString("user", "Empty User");
+        String token = prefs.getString("token", "Empty Token");
+        if (!user.contentEquals("Empty User")) {
+            JSONObject jsonObject = null;
+            try {
+                jsonObject = new JSONObject(user);
+                String username = jsonObject.getString("login");
+
+                Uri.Builder getObservations = new Uri.Builder();
+                getObservations.scheme("https").authority("www.inaturalist.org").appendPath(username).build();
+
+                new updateObservationList().execute(username);
+            }
+            catch (Exception e) {
+                Log.e(TAG, "Error converting to JSON Object");
+            }
+        }
+
         mObservations = ObservationList.get(getActivity()).getObservations();
 
         ObservationAdapter adapter = new ObservationAdapter(mObservations);
@@ -103,6 +133,15 @@ public class ObservationListFragment extends ListFragment {
             //solvedCheckBox.setChecked(.isSolved());
 
             return convertView;
+        }
+    }
+
+    private class updateObservationList extends AsyncTask<Object, Void, Void> {
+        @Override
+        protected Void doInBackground(Object... objects) {
+            // build url sendGet -> response
+            // jsondatato...
+            //
         }
     }
 }
